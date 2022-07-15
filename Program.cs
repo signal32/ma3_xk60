29439ef76c05c00a0d1666f6217e830b11da76a3
signal32 <55558643+signal32@users.noreply.github.com>
2022-07-15 10:25:57 +0100

    Setup

--- Program.cs
+++ Program.cs
@@ -1,9 +1,46 @@
-﻿namespace ma3_xk60.net
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ma3_xk60.net
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
+        
+        private static readonly CancellationTokenSource CancellationToken = new CancellationTokenSource();
+        
+        public static async Task Main(string[] args)
+        {
+            var xkeys = new XK_60_80.Xk60_80_();
+            SetupCancellation();
+            
+            Console.Out.WriteLine("Welcome. Detected {0}", xkeys.ConnectedDevices[0].ProductString);
+            
+            xkeys.ButtonChange += eventArgs =>
+            {
+                Console.WriteLine("foo");
+            };
+
+            await ExitRequest();
+            Console.WriteLine("Exiting...");
+        }
+
+        static void SetupCancellation()
+        {
+            Console.CancelKeyPress += (sender, args) =>
+            {
+                Console.WriteLine("Operation cancelled");
+                CancellationToken.Cancel();
+                args.Cancel = true;
+            };
+        }
+
+        async static Task ExitRequest()
+        {
+            while (!CancellationToken.IsCancellationRequested)
+            {
+                await Task.Delay(1000);
+            }
+        }
     }
-  }
 }
